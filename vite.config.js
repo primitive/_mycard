cea917302c3d6640c7d8515245b20a3621bf4bf4
Shaun Knowles <shaun@primitivedigital.uk>
2025-08-01 18:06:17 +0100

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
   base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My Card',
+        short_name: 'MyCard',
+        description: 'A simple digital business card',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#F2EFDE',
+        theme_color: '#133836',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
